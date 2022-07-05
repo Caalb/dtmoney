1a14c2faaa382defb1f365b566f34a934380d128
Caalb <carlosmunizfjunior@gmail.com>
2022-07-05 19:15:44 -0300

    create modal from react-modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from 'react-modal'
+import { useState } from "react";
+
+Modal.setAppElement('#root')
 
 export function App() {
+  const [isNewTransactionModelOpen, setIsNewTransactionModelOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModelOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModelOpen(false)
+  }
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModa={handleOpenNewTransactionModal}/>
       <Dashboard />
+      <Modal 
+        isOpen={isNewTransactionModelOpen} 
+        onRequestClose={handleCloseNewTransactionModal}>
+          <h2>Cadastrar transação</h2>
+        </Modal>
       <GlobalStyle />
     </>
   );
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,12 @@
+import { useEffect} from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
+  useEffect(() => {
+    api.get("transactions").then((response) => console.log(response.data));
+  }, []);
+
   return (
     <Container>
       <table>
@@ -17,14 +23,14 @@ export function TransactionsTable() {
           <tr>
             <td>Desenvolvimento de website</td>
             <td className="deposit">R$12.000</td>
-						<td>Desenvolvimento</td>
-						<td>20/02/2021</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
           </tr>
-          <tr >
+          <tr>
             <td>Aluguel</td>
             <td className="withdraw">R$1.100</td>
-						<td>Casa</td>
-						<td>17/02/2021</td>
+            <td>Casa</td>
+            <td>17/02/2021</td>
           </tr>
         </tbody>
       </table>
